36a29c791761883f9834f06b532c644cadb5ca47
Marc Jubero Silva <marc.jubero@a5s113pc43.fib.upc.es>
2015-04-21 14:15:55 +0200

    distributed groupy+backup & base code for gms2

--- 04_groupy/groupy.erl
+++ 04_groupy/groupy.erl
@@ -3,20 +3,34 @@
 
 % We use the name of the module (i.e. gms3) as the parameter Module to the start procedure. Sleep stands for up to how many milliseconds the workers should wait until the next message is sent.
 
-start(Module, Sleep) ->
-    P = worker:start("P1", Module, Sleep),
-    register(a, P), 
-    register(b, worker:start("P2", Module, P, Sleep)),
-    register(c, worker:start("P3", Module, P, Sleep)),
-    register(d, worker:start("P4", Module, P, Sleep)),
-    register(e, worker:start("P5", Module, P, Sleep)).
 
+
+start(Module, Sleep) ->
+    register(groupy, spawn(fun() -> init(Module, Sleep) end)).
+
+init(Module, Sleep) ->
+    %P = self(),
+    P = worker:start("P1", Module, Sleep),
+        
+    spawn('p2@127.0.0.1', fun() -> group_leader(whereis(user), self()), 
+                          worker:start("P2", Module, P, Sleep) 
+                          end),
+    spawn('p3@127.0.0.1', fun() -> group_leader(whereis(user), self()), 
+                          worker:start("P3", Module, P, Sleep) 
+                          end),
+    spawn('p4@127.0.0.1', fun() -> group_leader(whereis(user), self()), 
+                          worker:start("P4", Module, P, Sleep) 
+                          end),
+    spawn('p5@127.0.0.1', fun() -> group_leader(whereis(user), self()), 
+                          worker:start("P5", Module, P, Sleep) 
+                          end).
+    
 stop() ->
-    stop(a),
-    stop(b),
-    stop(c),
-    stop(d),
-    stop(e).
+    stop('p1@127.0.0.1'),
+    stop('p2@127.0.0.1'),
+    stop('p3@127.0.0.1'),
+    stop('p4@127.0.0.1'),
+    stop('p5@127.0.0.1').
 
 stop(Name) ->
     case whereis(Name) of
