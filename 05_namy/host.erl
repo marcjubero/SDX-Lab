29b88affd12f1b400b6ff2c89fe1ec914c821695
Marc Jubero Silva <marc.jubero@a5s113pc43.fib.upc.es>
2015-05-12 13:59:31 +0200

    deregister parent OK

--- 05_namy/host.erl
+++ 05_namy/host.erl
@@ -5,24 +5,25 @@ start(Name, Domain, Parent) ->
     register(Name, spawn(fun()-> init(Domain, Parent) end)).
 
 stop(Name) ->
-    Name ! stop,
+    Name ! {stop,Name},
     unregister(Name).
 
 init(Domain, Parent) ->
     io:format("Host: create domain ~w at ~w~n", [Domain, Parent]),
     Parent ! {register, Domain, {host, self()}},
-    host().
+    host(Parent).
 
-host() ->
+host(Parent) ->
     receive
         {ping, From} ->
             io:format("Host: Ping from ~w~n", [From]),
             From ! pong,
-            host();
-        stop ->
+            host(Parent);
+        {stop,Name} ->
             io:format("Host: Closing down~n", []),
+            Parent ! {deregister,Name},
             ok;
         Error ->
             io:format("Host: reception of strange message ~w~n", [Error]),
-            host()
+            host(Parent)
     end.
--- 05_namy/server.erl
+++ 05_namy/server.erl
@@ -13,35 +13,40 @@ stop() ->
 
 init() ->
     io:format("Server: create root domain~n"),
-    server([], 99999999999).
+    server([], 0, 0, 0).
 
 init(Domain, Parent) ->
     io:format("Server: create domain ~w at ~w~n", [Domain, Parent]),
     Parent ! {register, Domain, {domain, self()}},
-    server([], 99999999999).
+    server([], 0, Domain, Parent).
 
-server(Entries, TTL) ->
+server(Entries, TTL, Domain, Parent) ->
     receive
         {request, From, Req}->
             io:format("Server: received request to solve [~w]~n", [Req]),
             Reply = entry:lookup(Req, Entries),
             From ! {reply, Reply, TTL},
-            server(Entries, TTL);
+            server(Entries, TTL, Domain, Parent);
         {register, Name, Entry} ->
             Updated = entry:add(Name, Entry, Entries),
-            server(Updated, TTL);
+            server(Updated, TTL, Domain, Parent);
         {deregister, Name} ->
+	    io:format("Server: Deregister ~w~n", [Name]),
             Updated = entry:remove(Name, Entries),
-            server(Updated, TTL);
+            server(Updated, TTL, Domain, Parent);
         {ttl, Sec} ->
-            server(Entries, Sec);
+            server(Entries, Sec, Domain, Parent);
         status ->
             io:format("Server: List of DNS entries: ~w~n", [Entries]),
-            server(Entries, TTL);
+            server(Entries, TTL, Domain, Parent);
         stop ->
+	  case Parent == 0 of
+	     false ->
+		Parent ! {deregister,Domain}
+	    end,          
             io:format("Server: closing down~n", []),
             ok;
         Error ->
             io:format("Server: reception of strange message ~w~n", [Error]),
-            server(Entries, TTL)
+            server(Entries, TTL, Domain, Parent)
     end.
